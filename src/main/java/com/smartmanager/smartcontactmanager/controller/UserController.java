9fa41223437726d997471974b1f15314fb01ecda
Vivek Shinde <vivshind@publicisgroupe.net>
2023-04-12 10:07:39 +0530

    code added for tinymce in add_contact_form

--- src/main/java/com/smartmanager/smartcontactmanager/controller/UserController.java
+++ src/main/java/com/smartmanager/smartcontactmanager/controller/UserController.java
@@ -1,18 +1,29 @@
 package com.smartmanager.smartcontactmanager.controller;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.smartmanager.smartcontactmanager.dao.UserRepository;
 import com.smartmanager.smartcontactmanager.entities.Contact;
 import com.smartmanager.smartcontactmanager.entities.User;
+import com.smartmanager.smartcontactmanager.helper.Message;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/user")
@@ -50,11 +61,33 @@ public class UserController {
 
     // process the added contact for given user
     @PostMapping("/process-contact")
-    public String processContact(@ModelAttribute Contact contact, Principal principal) {
+    public String processContact(@ModelAttribute Contact contact, @RequestParam("profileImage") MultipartFile file,
+            Principal principal, HttpSession session) {
         try {
+            if (contact == null) {
+                throw new Exception("Add contact details");
+            }
             String username = principal.getName();
-
             User _user = _userRepository.getUserByUserName(username);
+
+            if (file.isEmpty()) {
+                System.out.println("File not provided");
+            } else {
+                String fileName = file.getOriginalFilename();
+                // save filename to DB
+                contact.setImageUrl(fileName);
+
+                // save file to static/img folder
+                // get the folder path
+                File folderPath = new ClassPathResource("static/image").getFile();
+
+                // create filename => append the filename to folderpath
+                Path filePath = Paths.get(folderPath.getAbsolutePath() + File.separator + fileName);
+
+                // save file to location
+                Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+                System.out.println("File is uploaded");
+            }
 
             // map user wrt contact
             contact.setUser(_user);
@@ -66,8 +99,14 @@ public class UserController {
             System.out.println(savedUser);
 
             System.out.println("Added into DB");
+
+            // Add message
+            session.setAttribute("message", new Message("Your contact is added", "success"));
+
             return "normal/add_contact_form";
+
         } catch (Exception e) {
+            session.setAttribute("message", new Message("Filed to update the contact try again", "danger"));
             return "normal/add_contact_form";
         }
     }
